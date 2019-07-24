30818a8be17c1b25afc691b01732272f18888ece
Sleeyax <yourd3veloper@gmail.com>
2019-07-24 20:30:41 +0200

    Added base64 conversion helpers

---
+++ mtgalib/Helpers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace mtgalib
+{
+    public class Helpers
+    {
+        public static string Base64Encode(string plaintext)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(plaintext);
+            return Convert.ToBase64String(bytes);
+        }
+
+        public static string Base64Decode(string encoded)
+        {
+            byte[] bytes = Convert.FromBase64String(encoded);
+            return Encoding.UTF8.GetString(bytes);
+        }
+    }
+}
