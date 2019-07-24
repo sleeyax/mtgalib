9be5724ef3214502c17035b23870705bef2d0993
Sleeyax <yourd3veloper@gmail.com>
2019-07-24 20:29:47 +0200

    Added example console app project

---
+++ mtgalib.example/Program.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mtgalib.example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Credentials credentials = new Credentials("token");
+            Credentials credentials = new Credentials("username", "password");
+            if (credentials.Verify())
+            {
+                Console.WriteLine(credentials.RefreshToken);
+                Console.WriteLine(credentials.AccessToken);
+                Console.WriteLine(credentials.DisplayName);
+                Console.WriteLine(credentials.PersonaId);
+            }
+            else
+            {
+                Console.WriteLine("Invalid credentials!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
