84b01e19431bebbd40dac3199308ade03104ffd7
Sleeyax <yourd3veloper@gmail.com>
2019-07-27 21:59:43 +0200

    Update test example

--- mtgalib.example/Program.cs
+++ mtgalib.example/Program.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.SqlServer.Server;
 
 namespace mtgalib.example
 {
@@ -11,7 +13,7 @@ namespace mtgalib.example
         static void Main(string[] args)
         {
             // Credentials credentials = new Credentials("token");
-            Credentials credentials = new Credentials("username", "password");
+            /*Credentials credentials = new Credentials("username", "password");
             bool verified = credentials.VerifyAsync().GetAwaiter().GetResult();
             if (verified)
             {
@@ -24,8 +26,29 @@ namespace mtgalib.example
             {
                 Console.WriteLine("Invalid credentials!");
             }
-
+            
             Console.ReadKey();
+            */
+
+           TcpConnTest();
+
+           Console.ReadKey();
+        }
+
+        static void TcpConnTest()
+        {
+            TcpConnection tcp = new TcpConnection();
+            tcp.Connect("client.arenagame-b.east.magic-the-gathering-arena.com", 9405);
+            tcp.OnConnected += () => Console.WriteLine("connected");
+            tcp.OnClose += (type) => Console.WriteLine("Disconnected");
+            tcp.OnMsgReceived += (bytes, i, arg3) => Console.WriteLine(Encoding.UTF8.GetString(bytes, i, arg3));
+           
+            while (tcp.Connected == false)
+            {
+
+            }
+            string msg = "{\"jsonrpc\":\"2.0\",\"method\":\"Derp\",\"params\":{},\"id\":\"56\"}";
+            tcp.SendAsync( Encoding.UTF8.GetBytes(msg));
         }
     }
 }
