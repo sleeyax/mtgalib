5269ec55ceb0c5d8adc82d2fdda33d24ff9117ef
Sleeyax <yourd3veloper@gmail.com>
2019-07-28 22:01:21 +0200

    Added version class to extract version info from string

---
+++ mtgalib/Version.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace mtgalib
+{
+    public class Version
+    {
+
+        public string Major { get; set; }
+        public string Minor { get; set; }
+        public string Patch { get; set; }
+        public string Meta { get; set; }
+
+        public Version(string version)
+        {
+            string[] splitted = version.Split('.');
+
+            if (splitted.Length > 4)
+                throw new ArgumentException("Version string can't contain more than 4 dots");
+
+            Major = splitted[0];
+            Minor = splitted[1];
+            Patch = splitted[2];
+            Meta  = splitted[3];
+        }
+
+    }
+}
