5bea8dcf4af26aea1d17ea1d8bdad3a1bd94dd91
Sleeyax <yourd3veloper@gmail.com>
2019-07-28 22:02:25 +0200

    Added JSON RPC response type class

---
+++ mtgalib/Server/JsonResponse.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+
+namespace mtgalib.Server
+{
+    public class JsonResponseResult
+    {
+        public string method { get; set; }
+        public JToken payload { get; set; }
+    }
+
+    public class JsonResponseError
+    {
+        public ServerErrorCode code { get; set; }
+        public string message { get; set; }
+        public Dictionary<string, object> data { get; set; }
+    }
+
+    public class JsonResponse
+    {
+        public string jsonrpc { get; set; }
+        public int id { get; set; }
+        public JsonResponseResult result { get; set; }
+        public JsonResponseError error { get; set; }
+    }
+}
