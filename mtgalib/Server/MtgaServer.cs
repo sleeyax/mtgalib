5815007a94b7580c9369e2da1be1f609d03e91da
Sleeyax <yourd3veloper@gmail.com>
2019-07-28 19:13:11 +0200

    Convert async callbacks to task methods

--- mtgalib/Server/MtgaServer.cs
+++ mtgalib/Server/MtgaServer.cs
@@ -1,4 +1,6 @@
-﻿using mtgalib.Player;
+﻿using System.Text;
+using System.Threading.Tasks;
+using mtgalib.Player;
 
 namespace mtgalib.Server
 {
@@ -15,9 +17,36 @@ namespace mtgalib.Server
             _port = playerEnvironment.Port;
         }
 
-        public void Connect()
+        public Task<bool> ConnectTask()
         {
             _tcpConnection.Connect(_host, _port);
+            TaskCompletionSource<bool> taskCompletionSource = new TaskCompletionSource<bool>();
+            _tcpConnection.OnConnected += b => taskCompletionSource.SetResult(b);
+
+            return taskCompletionSource.Task;
+        }
+
+        public Task<string> ReadResponseTask()
+        {
+            TaskCompletionSource<string> taskCompletionSource = new TaskCompletionSource<string>();
+            _tcpConnection.OnMsgReceived += (bytes, offset, length) =>
+            {
+                string msg = Encoding.UTF8.GetString(bytes, offset, length);
+                taskCompletionSource.SetResult(msg);
+            };
+
+            return taskCompletionSource.Task;
+        }
+
+        public void Disconnect()
+        {
+            if (_tcpConnection.Connected)
+                _tcpConnection.Close();
+        }
+
+        public void Send(string message)
+        {
+            _tcpConnection.SendAsync(Encoding.UTF8.GetBytes(message));
         }
     }
 }
