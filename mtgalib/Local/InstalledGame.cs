47b24f94072c626b493535f75a1418e6823fa44b
Sleeyax <yourd3veloper@gmail.com>
2019-07-29 23:04:08 +0200

    Added InstalledGame class

---
+++ mtgalib/Local/InstalledGame.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using Microsoft.Win32;
+
+namespace mtgalib.Local
+{
+    public class InstalledGame
+    {
+
+        private readonly string _gameDir;
+
+        public InstalledGame(string installationPath)
+        {
+            _gameDir = installationPath;
+        }
+
+        /// <summary>
+        /// Reads the refresh token from registry. Returns null if not found
+        /// </summary>
+        /// <returns></returns>
+        public string GetRefreshToken()
+        {
+            object rawRefreshToken = Registry.GetValue("HKEY_CURRENT_USER\\Software\\Wizards Of The Coast\\MTGA", "WAS-RefreshToken_h992839947", null);
+            return rawRefreshToken == null ? null : Encoding.UTF8.GetString((byte[]) rawRefreshToken);
+        }
+    }
+}
