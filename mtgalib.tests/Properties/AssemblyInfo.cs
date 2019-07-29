57908dbb8077c790c22107a8d15108478f38491d
Sleeyax <yourd3veloper@gmail.com>
2019-07-29 23:04:35 +0200

    Added unit tests

---
+++ mtgalib.tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("mtgalib.tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("mtgalib.tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0bb9ae96-d93b-4ec0-a764-d0b7df31ba28")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ mtgalib.tests/TestInstalledGame.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using mtgalib.Local;
+
+namespace mtgalib.tests
+{
+    [TestClass]
+    public class TestInstalledGame
+    { 
+
+        private InstalledGame _installedGame;
+
+        public TestInstalledGame()
+        {
+            _installedGame = new InstalledGame("D:\\Games\\MTGA");
+        }
+
+        [TestMethod]
+        public void ShouldReadRefreshTokenFromRegistry()
+        {
+            string refreshToken = _installedGame.GetRefreshToken();
+            Assert.IsNotNull(refreshToken, refreshToken);
+        }
+    }
+}
